58c46a0a2546a25a0d23054ae0c2a8f064e4addf
mengxing-n <57116364+mengxing-n@users.noreply.github.com>
2025-08-21 16:45:31 +0200

    update the initialization file.

--- aurelia/__init__.py
+++ aurelia/__init__.py
@@ -1,2 +1,6 @@
-import sys
-from PyCode import init_classes
+from .aurelia_arpes import Bands, Spec, ARPES
+from .aurelia_static_vars import mod, domain, experiment
+from . import aurelia_hdf5_saving as sv
+from .aurelia_plots import show_spectra as sh
+
+__version__ = "0.1.0"
